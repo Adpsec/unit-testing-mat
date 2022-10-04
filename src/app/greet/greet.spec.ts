cdf16d75477a977ed6b303f98836615203190d4a
Adpsec <asp.security@protonmail.com>
2022-10-04 09:43:28 -0500

    greet closed #3

---
+++ src/app/greet/greet.spec.ts
@@ -0,0 +1,8 @@
+import { greet } from "./greet";
+
+describe('greet', () => {
+    it("'should include the name in message'", () => {
+        expect(greet('mosh')).toBe('Welcome mosh');
+    });
+});
+
